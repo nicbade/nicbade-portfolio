807f82b541ba42e7e847ee11b96574b19b88c4a0
Nic Bade <chipchapmaniii@gmail.com>
2017-09-04 13:04:32 -0500

    set up heroku added email and phone links

--- server/app.js
+++ server/app.js
@@ -16,47 +16,47 @@ var oauthToken = process.env.GIT_TOKEN || require('./config.js').oauthToken;
 app.use(express.static('public'));
 
 var user_options = {
-  url: 'https://api.github.com/users/' + username,
-  headers: {
-    'User-Agent': 'request',
-    'Authorization': 'token ' + oauthToken
-  }
+    url: 'https://api.github.com/users/' + username,
+    headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token ' + oauthToken
+    }
 };
 
 // Moved API call into server to protect oAuthToken
-app.get('/github/user', function(req, res){
-  request(user_options, function (error, response, body) {
-    if (response && response.statusCode == 200) {
-      res.send(body);
-    } else {
-      res.sendStatus(500);
-    }
-  });
+app.get('/github/user', function(req, res) {
+    request(user_options, function(error, response, body) {
+        if (response && response.statusCode == 200) {
+            res.send(body);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 });
 
 var repo_options = {
-  url: 'https://api.github.com/users/' + username + '/repos',
-  headers: {
-    'User-Agent': 'request',
-    'Authorization': 'token ' + oauthToken
-  }
+    url: 'https://api.github.com/users/' + username + '/repos',
+    headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token ' + oauthToken
+    }
 };
 
 // Moved API call into server to protect oAuthToken
-app.get('/github/repos', function(req, res){
-  request(repo_options, function (error, response, body) {
-    if (response && response.statusCode == 200) {
-      res.send(body);
-    } else {
-      res.sendStatus(500);
-    }
-  });
+app.get('/github/repos', function(req, res) {
+    request(repo_options, function(error, response, body) {
+        if (response && response.statusCode == 200) {
+            res.send(body);
+        } else {
+            res.sendStatus(500);
+        }
+    });
 });
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+    res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 
 app.listen(port, function() {
-  console.log('localhost running on port', port);
+    console.log('localhost running on port', port);
 });
